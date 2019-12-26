22d38c7532cf915f41a0fca2e2baa8317b3c8f11
Shekey <ajdin_sah@hotmail.com>
2019-12-26 16:52:47 +0100

    removed unknown type

--- src/components/TypeDex.js
+++ src/components/TypeDex.js
@@ -7,10 +7,12 @@ const TypeDex = (props) => {
       {
         props.pokeTypes && props.pokeTypes.length > 0 ?
           props.pokeTypes.sort((a, b) => (a.name > b.name) ? 1 : -1).map(item => {
-           return ( <div key={item.name} className={`pokemon_item ${item.name}`}>
-              <div className={`pokemon_type_item`}>{item.name}</div>
-            </div>
-           )
+            if (item.name !== 'unknown') {
+              return (<div key={item.name} className={`pokemon_item ${item.name}`}>
+                <div className={`pokemon_type_item`}>{item.name}</div>
+              </div>
+              )
+            }
           })
           : null
       }
