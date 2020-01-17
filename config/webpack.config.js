839b260f0296c2e3f49040e8280d8b964db951a6
Shekey <ajdin_sah@hotmail.com>
2020-01-17 10:41:46 +0100

    webpack changed from all to async

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -264,7 +264,7 @@ module.exports = function(webpackEnv) {
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
-        chunks: 'all',
+        chunks: 'async',
         name: false,
       },
       // Keep the runtime chunk separated to enable long term caching
